11fa4db4979f9e7b5612a0fcaaeda36432c7384d
Nizzy <uanandaraja@gmail.com>
2025-11-01 21:18:56 +0700

    Remove DemoPlaceholder component

--- frontend/src/components/landing/index.ts
+++ frontend/src/components/landing/index.ts
@@ -1,6 +1,5 @@
 export { Hero } from "./Hero";
 export { Features } from "./Features";
-export { DemoPlaceholder } from "./DemoPlaceholder";
 export { LandingHeader } from "./LandingHeader";
 export { Footer } from "./Footer";
 
--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,10 +1,9 @@
-import { Hero, Features, DemoPlaceholder, Footer } from "@/components/landing";
+import { Hero, Features, Footer } from "@/components/landing";
 
 export function Home() {
   return (
     <>
       <Hero />
-      <DemoPlaceholder />
       <Features />
       <Footer />
     </>
